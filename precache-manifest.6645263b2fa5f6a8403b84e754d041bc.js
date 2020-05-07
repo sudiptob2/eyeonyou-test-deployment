cdface30b0fb78349fd13dae7e62b88158a2b437
sudipto baral <sudiptobaral.me@gmail.com>
2020-05-07 13:33:00 +0600

    1st test deployment

---
+++ precache-manifest.6645263b2fa5f6a8403b84e754d041bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d3a3b3fcb5967bcfb1dfb01d3ff6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1d749bbbfd050aa311",
+    "url": "/static/css/2.85d8d4b7.chunk.css"
+  },
+  {
+    "revision": "b61d9d6bcebcf6132a5b",
+    "url": "/static/css/main.8ece386d.chunk.css"
+  },
+  {
+    "revision": "eb1d749bbbfd050aa311",
+    "url": "/static/js/2.f2394b5d.chunk.js"
+  },
+  {
+    "revision": "a595b4f2762e24f4020ab9cb4b82cee1",
+    "url": "/static/js/2.f2394b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61d9d6bcebcf6132a5b",
+    "url": "/static/js/main.73d12d63.chunk.js"
+  },
+  {
+    "revision": "b2498c01ddbb8c0802f3",
+    "url": "/static/js/runtime-main.de722856.js"
+  },
+  {
+    "revision": "2340df7f9f9e2850376f1c1b28eb6e63",
+    "url": "/static/media/interface.2340df7f.svg"
+  },
+  {
+    "revision": "9850814c9816c439d29d17b7a38ed45e",
+    "url": "/static/media/interface_red.9850814c.svg"
+  },
+  {
+    "revision": "2c0c5093f8c8575e7dccc10c26032456",
+    "url": "/static/media/loading.2c0c5093.gif"
+  },
+  {
+    "revision": "6e328f631f1d46151eb38e81da5e53c0",
+    "url": "/static/media/power.6e328f63.svg"
+  },
+  {
+    "revision": "ced06f449545cc4da42f13a516a01f82",
+    "url": "/static/media/sadcat.ced06f44.svg"
+  },
+  {
+    "revision": "7cc442bbe30868adc30578d8223cd7cf",
+    "url": "/static/media/soundfile.7cc442bb.mp3"
+  }
+]);
